60db126a7d70683a0b10434dfd755d2654fe476f
lbmichael <michael@logitbot.com>
2016-04-13 02:28:12 -0400

    Add support for FX

--- miniconda/build.sh
+++ miniconda/build.sh
@@ -7,7 +7,7 @@
 export DOCKER_HOST=tcp://sysplex2.internal.logitbot.com:2376
 
 gcloud docker -a
-docker --tls build --pull --no-cache --rm --tag us.gcr.io/vnext-1254/miniconda2:0.2 .
-docker --tls tag us.gcr.io/vnext-1254/miniconda2:0.2 us.gcr.io/vnext-1254/miniconda2:latest
+docker --tls build --pull --no-cache --rm --tag us.gcr.io/vnext-1254/miniconda2:0.3 .
+docker --tls tag us.gcr.io/vnext-1254/miniconda2:0.3 us.gcr.io/vnext-1254/miniconda2:latest
 docker --tls push us.gcr.io/vnext-1254/miniconda2:latest
 
